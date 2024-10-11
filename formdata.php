b3be7e9f09271019014a121529906e1cbc148848
Warlin Ovalle <wso4@njit.edu>
2024-10-11 12:55:00 -0400

    updated formdata.php file

--- formdata.php
+++ formdata.php
@@ -11,9 +11,26 @@ $password = $_POST["password"];
 
 $passhash = password_hash($password, PASSWORD_DEFAULT);
 
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "test message";
+}
 $request = array();
-
 $request['type'] = "login";
 $request['username'] = $username;
 $request['password'] = $passhash;
+$request['message'] = $msg;
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+echo $argv[0]." END".PHP_EOL;
 
