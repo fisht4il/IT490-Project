66e6c33c652aaa71c27dbeae10944761669d9cbf
Mike <mb2297@njit.edu>
2024-10-15 01:13:46 -0400

    fix path script and add more php scripts

--- login.php
+++ login.php
@@ -10,15 +10,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
 
 	$request = array();
-	$request["type"] = "login";
-	$request["username"] = $username;
-	$request["password"] = $password;
-
+	$request['type'] = "login";
+	$request['username'] = $username;
+	$request['password'] = $password;
 
 	$response = $client->send_request($request);
 
 	echo "client received response: ".PHP_EOL;
 	print_r($response);
 	echo "\n\n";
+}
+
+else {
+	echo "Error";
 }
 ?>
