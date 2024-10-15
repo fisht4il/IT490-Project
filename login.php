6fd41591fd24476d053f5f64e14f4362140923ab
Mike <mb2297@njit.edu>
2024-10-14 23:25:24 -0400

    implement login with rabbitmq and add php scripts

--- login.php
+++ login.php
@@ -6,5 +6,19 @@ require_once('rabbitMQLib.inc');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$username = $_POST['username'];
 	$password = $_POST['password'];
+
+	$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+
+	$request = array();
+	$request["type"] = "login";
+	$request["username"] = $username;
+	$request["password"] = $password;
+
+
+	$response = $client->send_request($request);
+
+	echo "client received response: ".PHP_EOL;
+	print_r($response);
+	echo "\n\n";
 }
 ?>
