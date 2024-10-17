21d4a9a2048ed6ff7bedad77dd0826d45e8dbb81
Warlin Ovalle <wso4@njit.edu>
2024-10-17 19:39:04 -0400

    changes to index.html and login.php.

--- login.php
+++ login.php
@@ -3,32 +3,28 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
     $username = $_POST['username'];
     $password = $_POST['password'];
-
+    
     $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
 
-    $request = array();
-    $request['type'] = "login";
-    $request['username'] = $username;
-    $request['password'] = $password;
-    // $request['message'] = "test message";
+    $request = [
+        'type' => "login",
+        'username' => $username,
+        'password' => $password,
+        'message' => "Login Request"
+    ];
 
     $response = $client->send_request($request);
 
+    header('Content-Type: application/json');
 
-    // Output the response
-    echo "Client received response: <br>";
-    echo "<pre>";
-    print_r($response);
-    echo "</pre>";
-
-
+    echo json_encode($response);
 } else {
-	echo "Invalid request method.";
+    http_response_code(405);
+    echo json_encode([
+        "success" => false,
+        "message" => "Invalid request method."
+    ]);
 }
-
-?>
