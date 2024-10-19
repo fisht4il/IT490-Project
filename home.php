7ef08f3653a47f2aa2020f0a5b159526e3da809e
Mike <mb2297@njit.edu>
2024-10-19 16:03:31 -0400

    testing

--- home.php
+++ home.php
@@ -1,10 +1,10 @@
 <?php
+session_start();
 
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 	header("Location: index.html");
 	exit;
 }
-
 ?>
 
 <!DOCTYPE html>
--- login.php
+++ login.php
@@ -1,11 +1,7 @@
 <?php
-session_start();
 require_once('get_host_info.inc');
 require_once('path.inc');
 require_once('rabbitMQLib.inc');
-
-$valid_username = "user";
-$valid_password = "password";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$username = $_POST['username'];
@@ -20,26 +16,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 	$response = $client->send_request($request);
 
-	if ($username === $valid_username && $password === $valid_password) {
-		$_SESSION['loggedin'] = true;
-		$_SESSION['username'] = $username;
-		header("Location: home.php");
-		exit;
-	}
-
-	else {
-		echo "<p>Invalid username or password</p>";
-		echo "<p><a href=\"index.html\">Try again</a></p>";
-	}
-
 	echo "client received response: ".PHP_EOL;
 	print_r($response);
 	echo "\n\n";
 }
-
-else {
-	header("Location: index.html");
-	exit;
-}
-
 ?>
