d183e41e6cd979694601e9c2822c692e138722df
Mike <mb2297@njit.edu>
2024-10-19 15:45:36 -0400

    working on sessions

--- home.php
+++ home.php
@@ -1,1 +1,20 @@
+<?php
 
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+	header("Location: index.html");
+	exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Home</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<h1>Home</h1>
+		<a href="logout.php">Logout</a>
+	</body>
+</html>
