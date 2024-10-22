2d88bd0047c40d5982bc3b922c708011d6002698
Mike <90295919+fisht4il@users.noreply.github.com>
2024-10-22 18:30:29 -0400

    Update home.php

--- home.php
+++ home.php
@@ -1,20 +1,20 @@
 <?php
-session_start();
+session_start()
 
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-	header("Location: index.html");
-	exit;
+if(!(isset($_SESSION['username']) ) {
+        header ("Location:/html/index.html");
+        exit
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Home</title>
-		<link rel="stylesheet" href="style.css">
-	</head>
-	<body>
-		<h1>Home</h1>
-		<a href="logout.php">Logout</a>
-	</body>
+        <head>
+                <title><Home</title>
+                <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+                <h1>Welcome <?php echo $_SESSION['username'];?></h1>
+                <a href="/php/logout.php>Logout</a>
+        </body>
 </html>
