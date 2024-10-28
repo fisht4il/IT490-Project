9ff8f5ff392a24c06b1f27be9242d71063e08825
WarlinOvalle1 <158524327+WarlinOvalle1@users.noreply.github.com>
2024-10-28 10:13:34 -0400

    Update register.php
    Updated the registration file to look similar to the updated login file.

--- register.php
+++ register.php
@@ -1,7 +1,11 @@
 <?php
+
+session_start();
+
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
@@ -18,18 +22,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $response = $client->send_request($request);
 
-    header('Content-Type: application/json');
+    $response = json_decode(json: $response, associative: true);
 
-    echo json_encode($response);
+    if(isset($response['success']) && $response['success']) {
+            $_SESSION['username'] = $username;
+            echo json_encode(value: [
+                    "success" => true,
+                    "message" => "Registration successful.",
+                    "redirect" => "/php/home.php"
+            ]);
+            exit;
+    }
+
+    header(header: 'Content-Type: application/json');
+
+    echo json_encode(value: $response);
 
 } else {
-    http_response_code(405);
-    echo json_encode([
+    http_response_code(response_code: 405);
+    echo json_encode(value: [
         "success" => false,
         "message" => "Invalid request method."
     ]);
 }
 
-?>
 
+?>
 
