16046cb047c3942f0feb3d071a9c3caa61fb65d0
WarlinOvalle1 <158524327+WarlinOvalle1@users.noreply.github.com>
2024-10-31 10:55:20 -0400

    Update home.php
    Fixed some links to the other pages.

--- home.php
+++ home.php
@@ -16,7 +16,7 @@ if(!isset($_SESSION['username'])) {
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="../style.css">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home page</title>
@@ -24,7 +24,7 @@ if(!isset($_SESSION['username'])) {
 
     <body class="body-home">
         <header class="main-header">
-            <img src="kraken-svgrepo-com.png" alt="" class="logo-home">
+            <img src="../kraken-svgrepo-com.png" alt="" class="logo-home">
             <h1 class="page-title">IT-490-Project</h1>
             <nav class="header-nav">
                 <ul class="link-list">
