f0906d294fe1318dfac791ad4f44caa8b8e7df13
WarlinOvalle1 <158524327+WarlinOvalle1@users.noreply.github.com>
2024-10-30 23:20:24 -0400

    Update home.php
    Duplicated the header content of the signup and index. Added a kraken title and logo.

--- home.php
+++ home.php
@@ -8,13 +8,23 @@ if(!isset($_SESSION['username'])) {
 }
 
 ?>
-<html>
-    <head>
-        <link rel="stylesheet" href="style.css">
-    </head>
 
-    <body>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home page</title>
+</head>
+
+    <body class="body-home">
         <header class="main-header">
+            <img src="kraken-svgrepo-com.png" alt="" class="logo-home">
             <h1 class="page-title">IT-490-Project</h1>
             <nav class="header-nav">
                 <ul class="link-list">
@@ -40,6 +50,7 @@ if(!isset($_SESSION['username'])) {
             </nav>
         </header>
         <section class="section-text">
+                <h2 class="home-title">KRAKEN</h2>
                 <p class="place-text">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi cupiditate magnam vero dignissimos dolorum inventore ratione ipsum quisquam porro odit non neque, rem doloremque molestiae quaerat explicabo at aspernatur hic.
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, aliquid! Porro explicabo dignissimos officia officiis, dolorum harum ipsa tenetur accusantium, nihil fuga deserunt ut, consectetur fugiat ipsam nobis rem vel.
@@ -51,4 +62,5 @@ if(!isset($_SESSION['username'])) {
             <p class="copyright">&copy; 2024. Copyright by IT-490-Project</p>
         </footer>
     </body>
+
 </html>
