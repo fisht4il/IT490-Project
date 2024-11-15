f0b724c89b0de2b3bceb7cf6bc19f469f9989b39
Rishabh Sinha <106822997+rishabh-sinha-njit@users.noreply.github.com>
2024-11-15 13:57:46 -0500

    Add files via upload
    Created Data Collection

---
+++ data_collection.php
@@ -0,0 +1,33 @@
+<?php
+abstract class trading_data
+{
+    protected $query, $query_result; 
+    public $trade_data_rec = array();
+    public $metadata;
+    protected function construct_trading_data_collector($query, $query_result)
+    {
+        $this->query = $query;
+        $this->query_result = $query_result;
+        $this->get_query_results_metadata();
+        $this->parse_result_records();
+    }
+
+    protected abstract function get_record_object_id();
+    protected function get_query_results_metadata()
+    {
+        $this->metadata = new meta_data_record($this->query_result->{'Data Meta'});
+    }
+
+    protected function parse_result_records()
+    {
+        $record_object = get_class(get_object_vars($this->query_result->{$this->get_record_object_id()}));
+        print_r($record_object);
+        $query_results_array = (array)$record_object;
+        foreach($query_results_array as $query_result_record)
+        {
+            $this_record = new trading_data_record($query_result_record);
+            array_push($this->trade_data_rec, $this_record);
+        }
+    }
+}
+?>
