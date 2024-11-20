8ab45245e3ffc710ea4af253a45dad53296d57e5
WarlinOvalle1 <158524327+WarlinOvalle1@users.noreply.github.com>
2024-11-20 15:25:49 -0500

    Update recommendations.php
    Reintegrated the navbar into the main pages so that they change depending on the page the user is on.

--- php/recommendations.php
+++ php/recommendations.php
@@ -35,6 +35,21 @@ if (!$response['success']) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recommendations</title>
 </head>
+
+<header class="main-header">
+    <a href="home.php">
+        <img src="../media/logo.png" alt="Logo" class="logo-home">
+    </a>
+    <?php echo "Welcome, " . htmlspecialchars($_SESSION['username']) . "."; ?>
+    <!-- <h1 class="page-title">IT-490-Project</h1> -->
+    <nav class="header-nav">
+        <a href="home.php" class="placeholder-link">Dashboard</a>
+        <a href="trades.php" class="placeholder-link">Trades</a>
+        <a href="orders.php" class="placeholder-link">Orders</a>
+        <a href="logout.php" class="placeholder-link">Logout</a>
+    </nav>
+</header>
+
 <body class="body-home">
     <?php include 'partials/navbar.php'; ?>
 
