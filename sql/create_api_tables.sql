cfcaf5da2e102c929bbd2d45d83598447e9fae27
yumis56 <ys56@njit.edu>
2024-11-21 11:51:54 -0500

    added limit_orders table, usable for both buying and selling.

--- sql/create_api_tables.sql
+++ sql/create_api_tables.sql
@@ -13,4 +13,21 @@ CREATE TABLE stock_prices (
     INDEX idx_symbol (symbol)
 );
 
+--
+
+CREATE TABLE limit_orders (
+    id INT AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    symbol VARCHAR(10) NOT NULL,
+    request_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    close_date DATETIME DEFAULT NULL. --close is for both 'filled' and 'canceled' status
+    request_price DECIMAL (10,4) NOT NULL,
+    request_quantity INT UNSIGNED NOT NULL,
+    order_type ENUM('buy', 'sell') NOT NULL,
+    status ENUM('open', 'filled', 'canceled') DEFAULT 'open',
+    PRIMARY KEY (user_id, symbol, id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
 --will probably need to update this file later with more tables
