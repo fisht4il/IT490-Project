136a212042952ba5f0e7d7a2cdea1db318f18ed0
yumis56 <ys56@njit.edu>
2024-11-21 11:56:28 -0500

    added default fund value 0.00

--- sql/create_table_wallet.sql
+++ sql/create_table_wallet.sql
@@ -1,6 +1,6 @@
 CREATE TABLE user_wallet (
     user_id INT NOT NULL,
-    current_balance DECIMAL (15, 2),
+    current_balance DECIMAL (15, 2) DEFAULT 0.00,
     PRIMARY KEY (user_id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
