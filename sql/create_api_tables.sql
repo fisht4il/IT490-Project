ad6ad683ab17ca4414ff8c69b808b2e4dffd8cef
yumis56 <ys56@njit.edu>
2024-11-21 12:39:38 -0500

    added stock_quotes table that is used for most recent price of a stock, not the overall history

--- sql/create_api_tables.sql
+++ sql/create_api_tables.sql
@@ -1,6 +1,7 @@
 -- referenced source for structure primary key structure: 
 -- https://medium.com/@herefindalex/designing-an-efficient-stock-price-database-from-basic-structure-to-optimization-strategies-44ba2c01fae9#:~:text=Use%20symbol%20as%20the%20primary,and%20test%20symbols%20like%20BLIST.
 
+-- this one is for overall history
 CREATE TABLE stock_prices (
     symbol VARCHAR(10) NOT NULL,
     date DATE NOT NULL,
@@ -11,6 +12,22 @@ CREATE TABLE stock_prices (
     volume INT NOT NULL,
     PRIMARY KEY (symbol, date),
     INDEX idx_symbol (symbol)
+);
+
+--
+-- this one is for single most recent price of a stock
+
+CREATE TABLE stock_quotes (
+    symbol VARCHAR(10) PRIMARY KEY UNIQUE NOT NULL,
+    open DECIMAL(10, 4) NOT NULL,
+    high DECIMAL(10, 4) NOT NULL,
+    low DECIMAL(10, 4) NOT NULL,
+    price DECIMAL (15, 4) NOT NULL,
+    volume INT NOT NULL,
+    latest_trading_day DATETIME,
+    prev_close DECIMAL(10, 4),
+    change DECIMAL(10, 4),
+    change_percent VARCHAR(10) --there is a percent symbol. I do not think we are doing many calculations, hence storing as varchar.
 );
 
 --
