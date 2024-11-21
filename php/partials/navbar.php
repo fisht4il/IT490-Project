a34eb2a112e63bb2d21faef55f4a407b28a7524e
WarlinOvalle1 <158524327+WarlinOvalle1@users.noreply.github.com>
2024-11-21 10:12:03 -0500

    Update navbar.php
    Search in the navbar just in case it is needed. It is commented out.

--- php/partials/navbar.php
+++ php/partials/navbar.php
@@ -1,3 +1,4 @@
+
 <header class="main-header">
     <a href="home.php">
         <img src="../media/logo.png" alt="Logo" class="logo-home">
@@ -5,6 +6,10 @@
     <?php echo "Welcome, " . htmlspecialchars($_SESSION['username']) . "."; ?>
     <!-- <h1 class="page-title">IT-490-Project</h1> -->
     <nav class="header-nav">
+        <!--<form action="" id="searchForm" role="search" class="search">
+            <input type="search" id="searchInput" class="search-input" placeholder="Search...">
+            <input type="submit" class="submit-search">
+        </form>-->
         <a href="recommendations.php" class="nav-link">Recommendations</a>
         <a href="trades.php" class="nav-link">Trades</a>
         <a href="orders.php" class="nav-link">Orders</a>
