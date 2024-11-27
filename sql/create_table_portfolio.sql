a68c1855a58310a84caed291a7a3f1f547c40d0f
yumis56 <ys56@njit.edu>
2024-11-27 12:54:04 -0500

    fixed syntax for user_portfolio

--- sql/create_table_portfolio.sql
+++ sql/create_table_portfolio.sql
@@ -2,8 +2,12 @@ CREATE TABLE user_portfolio (
     user_id INT NOT NULL,
     symbol VARCHAR(10) NOT NULL,
     quantity INT UNSIGNED DEFAULT 0,
-    average_price (DECIMAL 15, 4) DEFAULT 0.00, -- average that user spent, not overall global data. base on 'filled' orders.
-    PRIMARY_KEY(user_id, symbol),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (symbol) REFERENCES stock_quotes(symbol),
+    average_price DECIMAL (15, 4) DEFAULT 0.00,
+    PRIMARY KEY (user_id, symbol),
+    FOREIGN KEY (user_id) REFERENCES logindb.users(id),
+    FOREIGN KEY (symbol) REFERENCES stock_quotes(symbol)
 );
+
+
+
+
