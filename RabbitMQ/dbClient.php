b4e7e58f9e95beb9b0d4fdaa48c82b4b27857105
fisht4il <idksomethingaboutclocks1@gmail.com>
2024-11-30 17:08:25 -0500

    add stockdb to dbClient.php

--- RabbitMQ/dbClient.php
+++ RabbitMQ/dbClient.php
@@ -3,5 +3,6 @@ return [
     'DBHOST' => '127.0.0.1',
     'DBUSER' => 'rabbit',
     'DBPASSWORD'=> 'rabbitIT490!',
-    'LOGINDATABASE' => 'logindb'
+    'LOGINDATABASE' => 'logindb',
+    'STOCKDATABASE' => 'stockdb'
 ];
