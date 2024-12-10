ae21ee54e05d8455a7c11e75112190c14d12bca0
Warlin Ovalle <warlinovalle95@gmail.com>
2024-12-10 18:04:36 -0500

    changes to the navbar and the styling page.

--- php/partials/navbar.php
+++ php/partials/navbar.php
@@ -1,13 +1,18 @@
+
 <header class="main-header">
-        <a href="home.php">
-		 <img src="../media/logo.png" alt="Logo" class="nav-logo">
-        </a>
-        <?php echo "Welcome, " . htmlspecialchars($_SESSION['username']) . "."; ?>
-        <!-- <h1 class="nav-title">IT-490-Project</h1> -->
-	<nav class="header-nav">
-		<a href="funds.php" class="nav-link">Funds</a>
-		<a href="trades.php" class="nav-link">Trades</a>
-            	<a href="orders.php" class="nav-link">Orders</a>
-            	<a href="logout.php" class="nav-link">Logout</a>
-        </nav>
+    <a href="home.php">
+        <img src="../media/logo.png" alt="Logo" class="nav-logo">
+    </a>
+    <?php echo "Welcome, " . htmlspecialchars($_SESSION['username']) . "."; ?>
+
+    <nav class="header-nav">
+        <!--<form action="" id="searchForm" role="search" class="search">
+            <input type="search" id="searchInput" class="search-input" placeholder="Search...">
+            <input type="submit" class="submit-search">
+        </form>-->
+        <a href="funds.php" class="nav-link">Funds</a>
+        <a href="trades.php" class="nav-link">Trades</a>
+        <a href="orders.php" class="nav-link">Orders</a>
+        <a href="logout.php" class="nav-link">Logout</a>
+    </nav>
 </header>
