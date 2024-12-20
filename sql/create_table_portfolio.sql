d0f84d88f0237b30097f7534e61bd29e667a796a
fisht4il <idksomethingaboutclocks1@gmail.com>
2024-12-20 13:15:15 -0500

    update portfolio table

--- sql/create_table_portfolio.sql
+++ sql/create_table_portfolio.sql
@@ -5,7 +5,7 @@ CREATE TABLE user_portfolio (
     average_price DECIMAL (15, 4) DEFAULT 0.00,
     PRIMARY KEY (user_id, symbol),
     FOREIGN KEY (user_id) REFERENCES logindb.users(id),
-    FOREIGN KEY (symbol) REFERENCES popular_stocks(symbol)
+    FOREIGN KEY (symbol) REFERENCES stock_quotes(symbol)
 );
 
 
